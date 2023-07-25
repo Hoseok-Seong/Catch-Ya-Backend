cf986cad3f49077df74afeeaf88f238ee021d13a
Hoseok Seong <shsshs0125@naver.com>
2023-07-25 15:49:15 +0900

    Feat: 신고 목록 보기 기능 완료

--- src/main/java/com/example/catchya/report/controller/ReportController.java
+++ src/main/java/com/example/catchya/report/controller/ReportController.java
@@ -1,12 +1,14 @@
 package com.example.catchya.report.controller;
 
 import com.example.catchya.global.security.MyUserDetails;
+import com.example.catchya.report.dto.ReportFindReq;
 import com.example.catchya.report.dto.ReportInsertReq;
 import com.example.catchya.report.service.ReportService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +22,11 @@ public class ReportController {
     public ResponseEntity<?> insertReport(@AuthenticationPrincipal MyUserDetails myUserDetails,
                                           @RequestBody @Valid ReportInsertReq reportInsertReq) {
         return reportService.insertReport(myUserDetails, reportInsertReq);
+    }
+
+    @GetMapping("/api/reports")
+    public ResponseEntity<?> getReports(@AuthenticationPrincipal MyUserDetails myUserDetails,
+                                        @RequestBody @Valid ReportFindReq reportFindReq) {
+        return reportService.getReports(myUserDetails, reportFindReq);
     }
 }
--- src/main/java/com/example/catchya/report/service/ReportService.java
+++ src/main/java/com/example/catchya/report/service/ReportService.java
@@ -1,6 +1,8 @@
 package com.example.catchya.report.service;
 
 import com.example.catchya.global.security.MyUserDetails;
+import com.example.catchya.report.dto.ReportFindReq;
+import com.example.catchya.report.dto.ReportFindResp;
 import com.example.catchya.report.dto.ReportInsertReq;
 import com.example.catchya.report.dto.ReportInsertResp;
 import com.example.catchya.report.entity.Report;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -27,5 +30,17 @@ public class ReportService {
         Report report = reportRepository.save(reportInsertReq.toEntity());
 
         return ResponseEntity.ok().body(new ReportInsertResp(report));
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<?> getReports(MyUserDetails myUserDetails,
+                                        ReportFindReq reportFindReq) {
+        if(!Objects.equals(reportFindReq.username(), myUserDetails.getUsername())) {
+            return ResponseEntity.badRequest().body("유저 이름이 일치하지 않습니다");
+        }
+
+        List<Report> report = reportRepository.findByUsername(reportFindReq.username());
+
+        return ResponseEntity.ok().body(new ReportFindResp(report));
     }
 }
