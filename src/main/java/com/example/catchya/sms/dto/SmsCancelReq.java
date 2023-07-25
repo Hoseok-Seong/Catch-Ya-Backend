ae69f0c6390d577b67b83dfb90676866a404f9f4
Hoseok Seong <shsshs0125@naver.com>
2023-07-25 16:43:12 +0900

    Feat: valid 처리 완료

--- src/main/java/com/example/catchya/sms/dto/SmsCancelReq.java
+++ src/main/java/com/example/catchya/sms/dto/SmsCancelReq.java
@@ -1,8 +1,10 @@
 package com.example.catchya.sms.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class SmsCancelReq {
+    @NotBlank(message = "requestId를 입력해주세요")
     private String requestId;
 }
--- src/main/java/com/example/catchya/sms/dto/SmsInsertReq.java
+++ src/main/java/com/example/catchya/sms/dto/SmsInsertReq.java
@@ -1,5 +1,6 @@
 package com.example.catchya.sms.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @Getter
 public class SmsInsertReq {
     private String additionalContent;
+    @NotBlank(message = "예약시간을 입력해주세요")
     private String reserveTime;
+    @NotBlank(message = "messageId를 입력해주세요")
     private Long messageId;
 
     @AllArgsConstructor
