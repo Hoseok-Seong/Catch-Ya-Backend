78f438af0d85823822a59606eec8fe815ef80541
Hoseok Seong <shsshs0125@naver.com>
2023-07-25 15:19:06 +0900

    Feat: 로그인 기능 완료

--- src/main/java/com/example/catchya/user/controller/UserController.java
+++ src/main/java/com/example/catchya/user/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.example.catchya.user.controller;
 
+import com.example.catchya.user.dto.UserLoginReq;
+import com.example.catchya.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid UserLoginReq userLoginReq) {
+        return userService.login(userLoginReq);
+    }
 }
--- src/main/java/com/example/catchya/user/service/UserService.java
+++ src/main/java/com/example/catchya/user/service/UserService.java
@@ -1,4 +1,31 @@
 package com.example.catchya.user.service;
 
+import com.example.catchya.global.jwt.MyJwtProvider;
+import com.example.catchya.user.dto.UserLoginReq;
+import com.example.catchya.user.dto.UserLoginResp;
+import com.example.catchya.user.entity.User;
+import com.example.catchya.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<?> login(UserLoginReq userLoginReq) {// 60Byte
+        User user = userRepository
+                .findByUsername(userLoginReq.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다"));
+        if (passwordEncoder.matches(userLoginReq.getPassword(), user.getPassword())) {
+            String jwt = MyJwtProvider.create(user);
+            return ResponseEntity.ok().header(MyJwtProvider.HEADER, jwt).body(new UserLoginResp(user));
+        }
+        throw new RuntimeException("패스워드 유효성 실패");
+    }
 }
