86cb380df67cf48f127c13514342e193ed6b01d9
Hoseok Seong <shsshs0125@naver.com>
2023-07-25 14:55:58 +0900

    Init

---
+++ src/main/java/com/example/catchya/CatchYaApplication.java
@@ -0,0 +1,13 @@
+package com.example.catchya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchYaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchYaApplication.class, args);
+    }
+
+}
