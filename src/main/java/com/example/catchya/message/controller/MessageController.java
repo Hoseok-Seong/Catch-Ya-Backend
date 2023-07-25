b0f30732346784c485a039e23ca202f7048c2a28
Hoseok Seong <shsshs0125@naver.com>
2023-07-25 15:59:19 +0900

    Feat: 저장된 문자 찾기 기능 완료

--- src/main/java/com/example/catchya/message/controller/MessageController.java
+++ src/main/java/com/example/catchya/message/controller/MessageController.java
@@ -1,12 +1,14 @@
 package com.example.catchya.message.controller;
 
 import com.example.catchya.global.security.MyUserDetails;
+import com.example.catchya.message.dto.MessageFindReq;
 import com.example.catchya.message.dto.MessageInsertReq;
 import com.example.catchya.message.service.MessageService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,11 @@ public class MessageController {
     public ResponseEntity<?> insertMessage(@AuthenticationPrincipal MyUserDetails myUserDetails,
                                            @RequestBody @Valid MessageInsertReq messageInsertReq) {
         return messageService.insertMessage(myUserDetails, messageInsertReq);
+    }
+
+    @GetMapping("/api/messages")
+    public ResponseEntity<?> getMessages(@AuthenticationPrincipal MyUserDetails myUserDetails,
+                                         @RequestBody @Valid MessageFindReq messageFindReq) {
+        return messageService.getMessages(myUserDetails, messageFindReq);
     }
 }
--- src/main/java/com/example/catchya/message/service/MessageService.java
+++ src/main/java/com/example/catchya/message/service/MessageService.java
@@ -1,6 +1,8 @@
 package com.example.catchya.message.service;
 
 import com.example.catchya.global.security.MyUserDetails;
+import com.example.catchya.message.dto.MessageFindReq;
+import com.example.catchya.message.dto.MessageFindResp;
 import com.example.catchya.message.dto.MessageInsertReq;
 import com.example.catchya.message.dto.MessageInsertResp;
 import com.example.catchya.message.entity.Message;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -28,4 +31,17 @@ public class MessageService {
 
         return ResponseEntity.ok().body(new MessageInsertResp(message));
     }
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<?> getMessages(MyUserDetails myUserDetails,
+                                         MessageFindReq messageFindReq) {
+        if(!Objects.equals(messageFindReq.username(), myUserDetails.getUsername())) {
+            return ResponseEntity.badRequest().body("유저 이름이 일치하지 않습니다");
+        }
+
+        List<Message> messages = messageRepository.findByUsername(messageFindReq.username());
+
+        return ResponseEntity.ok().body(new MessageFindResp(messages));
+    }
+
 }
